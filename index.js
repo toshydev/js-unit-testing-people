2aabe15baa2d1762ef519538a34e7c6d55eca20d
Anton Roters <antonroters@hotmail.com>
2023-03-03 12:40:42 +0100

    finish tests and functions

--- index.js
+++ index.js
@@ -2,18 +2,59 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => {
+    return `${person.firstName} ${person.lastName}`;
+  });
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => {
+    return `${person.lastName} (${person.age})`;
+  });
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => {
+    return person.age === age;
+  });
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => {
+      return person.age > age;
+    })
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => {
+    return person.lastName === lastName;
+  });
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((person) => {
+    return person.id === id;
+  });
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age);
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  return people
+    .slice()
+    .sort((personA, personB) => {
+      if (personA.age > personB.age) {
+        return 11;
+      }
+      if (personA.age < personB.age) {
+        return -1;
+      }
+      return 0;
+    })
+    .map((person) => person.lastName);
+}
